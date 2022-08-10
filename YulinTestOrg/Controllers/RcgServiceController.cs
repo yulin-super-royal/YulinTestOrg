cbc105d7600d70ac418dac45a9bb9e8d7c87a084
yulin <yulin@super-royal.com>
2022-08-10 10:03:20 +0800

    [add] 加入前端lib

---
+++ YulinTestOrg/Controllers/RcgServiceController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using YulinTestOrg.Data;
+using YulinTestOrg.Interface;
+
+namespace YulinTestOrg.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    [Authorize]
+    public class RcgServiceController : ControllerBase
+    {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly IRcgApiService _rcgApiService;
+
+        public RcgServiceController(UserManager<ApplicationUser> userManager,
+                                    IRcgApiService rcgApiService)
+        {
+            _userManager = userManager;
+            _rcgApiService = rcgApiService;
+        }
+
+        [HttpPost]
+        [Route("LoginToRcg")]
+        public async Task<IActionResult> LoginToRcg()
+        {
+            var userName = HttpContext.User.Identity.Name;
+            var result = await _rcgApiService.Login(userName);
+            return Ok(result);
+        }
+    }
+}
